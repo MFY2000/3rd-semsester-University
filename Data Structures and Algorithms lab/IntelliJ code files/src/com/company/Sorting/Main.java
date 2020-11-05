6c2ce3de4cc26170f86cfce86a7770853d6ae1b7
MFY2000 <fa19bsse0014@maju.edu.pk>
2020-11-05 23:04:08 +0500

    5/11/2020 10:15 pm last

--- Data Structures and Algorithms lab/IntelliJ code files/src/com/company/Sorting/Main.java
+++ Data Structures and Algorithms lab/IntelliJ code files/src/com/company/Sorting/Main.java
@@ -1,31 +1,60 @@
 package com.company.Sorting;
 
 import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         int array[] = {5,6,1,1,8,9,3,5};
+        int insertion,seletion;
 
         Sorting ob = new Sorting();
+        insertion = ob.SelectionSorting(array);
+        seletion = ob.InsertionSorting(array);
 
-        ob.SelectionSorting(array);
-        ob.InsertionSorting(array);
+        String temp = "The best Sorting technique is can be choose by the comparing the count of inner loop\nso the best way is ";
 
+
+
+        temp += (insertion < seletion?"Selection Sorting":"Insertion Sorting");
+
+        System.out.println(temp);
     }
 }
 class Sorting{
 
-    void SelectionSorting(int arr[]){
+    int InsertionSorting(int arr[]) {
         int length = arr.length,
-            swap,
-            countloop = 0;
-        System.out.println("----------------------------------------- Selection Sorting ----------------------------------------- ");
+        countloop = 0;
+        System.out.println("----------------------------------------- Insertion Sorting ----------------------------------------- ");
+
+        for (int i = 1; i < (length); ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+//                System.out.println("j = "+j+",key = "+key+",Check("+arr[j]+"<"+key+") => "+(j >= 0 && arr[j] > key));
+                arr[j + 1] = arr[j];
+                j = j - 1;
+                countloop++;
+            }
+            arr[j + 1] = key;
+//            System.out.println(Arrays.toString(arr));
+//            System.out.println("----------------------------------------- i = " + i + " Complete -----------------------------------------");
+
+        }
         System.out.println(Arrays.toString(arr));
+        System.out.println("Number of time value inner loop works: "+countloop);
+        return countloop;
+    }
+
+    int SelectionSorting(int arr[]){
+        int length = arr.length,
+                swap,
+                countloop = 0;
+        System.out.println("----------------------------------------- Selection Sorting ----------------------------------------- ");
 
         for (int i = 0; i < (length-1); i++) {
             int minIndex = i;
             for (int j = i+1; j < length; j++) {
-                System.out.println("j = "+j+",minIndex = "+minIndex+",minValue = "+arr[minIndex]+",Check("+arr[j]+"<"+arr[minIndex]+") => "+(arr[j] < arr[minIndex]));
+//                System.out.println("j = "+j+",minIndex = "+minIndex+",minValue = "+arr[minIndex]+",Check("+arr[j]+"<"+arr[minIndex]+") => "+(arr[j] < arr[minIndex]));
                 if (arr[j] < arr[minIndex]) {
                     minIndex = j;
                 }
@@ -34,35 +63,12 @@ class Sorting{
             swap = arr[minIndex];
             arr[minIndex] = arr[i];
             arr[i] = swap;
-            System.out.println("----------------------------------------- i = "+i+" Complete -----------------------------------------");
-            System.out.println(Arrays.toString(arr));
-
+//            System.out.println("----------------------------------------- i = "+i+" Complete -----------------------------------------");
+//            System.out.println(Arrays.toString(arr));
         }
-
-        System.out.println("Number of time value inner loop works: "+countloop);
-    }
-
-    void InsertionSorting(int arr[]) {
-        int length = arr.length,
-        countloop = 0;
-        System.out.println("----------------------------------------- Selection Sorting ----------------------------------------- ");
         System.out.println(Arrays.toString(arr));
-
-        for (int i = 1; i < (length); ++i) {
-            int key = arr[i];
-            int j = i - 1;
-            while (j >= 0 && arr[j] > key) {
-                System.out.println("j = "+j+",key = "+key+",Check("+arr[j]+"<"+key+") => "+(j >= 0 && arr[j] > key));
-                arr[j + 1] = arr[j];
-                j = j - 1;
-                countloop++;
-            }
-            arr[j + 1] = key;
-            System.out.println(Arrays.toString(arr));
-            System.out.println("----------------------------------------- i = " + i + " Complete -----------------------------------------");
-
-        }
         System.out.println("Number of time value inner loop works: "+countloop);
+        return countloop;
     }
 
 
