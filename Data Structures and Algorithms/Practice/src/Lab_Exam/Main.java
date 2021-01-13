08f99ff1f3a6d18d40c0fff12f4c70af6cf12dd0
MFY2000 <FA19BSSE0014@MAJU.EDU.PK>
2021-01-13 16:01:29 +0500

    4:01 pm 13/1/2020

--- Data Structures and Algorithms/Practice/src/Lab_Exam/Main.java
+++ Data Structures and Algorithms/Practice/src/Lab_Exam/Main.java
@@ -4,13 +4,13 @@ import Lab_Exam.Search.Binary;
 import Lab_Exam.Search.Linear;
 import Lab_Exam.Sorting.Bubble;
 import Lab_Exam.Sorting.Insertion;
+import Lab_Exam.Sorting.Selection;
 
 public class Main {
-
     public static void main(String[] args) {
         int[] array = new int[]{7, 98, 6, 12, 84, 2, 68, 8,84,84,55};
 
-        Insertion Search = new Insertion(array);
+        Selection Search = new Selection(array);
         System.out.println(Search.toString());
 
     }
---
+++ Data Structures and Algorithms/Practice/src/Lab_Exam/Sorting/Selection.java
@@ -0,0 +1,53 @@
+package Lab_Exam.Sorting;
+
+import java.util.Arrays;
+
+public class Selection {
+    private int[] array_Search;
+    private int key;
+    private int lenght;
+    private boolean flag;
+
+    public Selection(int[] array_search) {
+        setArray_Search(array_search);
+        setLenght();
+        setFlag(true);
+
+    }
+
+    public int[] getArray_Search() { return array_Search; }
+    public String getArray(){return Arrays.toString(getArray_Search());}
+    public int getArray_index(int index) { return array_Search[index]; }
+    public int getKey() { return key; }
+    public int getLenght() { return lenght; }
+    public boolean isFlag() { return flag; }
+
+    public void setArray_Search(int[] array_Search) { this.array_Search = array_Search; }
+    public void setArray_index(int index,int value) { array_Search[index] = value; }
+    public void setKey(int key) { this.key = key; }
+    public void setLenght() { this.lenght = (getArray_Search()).length; }
+    public void setFlag(boolean flag) { this.flag = flag; }
+
+    public void Sort(){
+    for (int i = 0; i < getLenght()-1; i++) {
+        int minIndex = i;
+        for (int j = i+1; j < getLenght(); j++)
+            if (getArray_index(j) < getArray_index(minIndex))
+                minIndex = j;
+
+        Swap(minIndex,i);
+        }
+    }
+
+
+    public void Swap(int j,int i){
+        int swap = getArray_index(j);
+        setArray_index(j,getArray_index(i));
+        setArray_index(i,swap);
+    }
+
+    public String toString(){
+        Sort();
+        return getArray();
+    }
+}
