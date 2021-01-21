9dfb1a9295bb92bae729d1392fee7fcc865fe664
MFY2000 <FA19BSSE0014@MAJU.EDU.PK>
2021-01-21 12:28:14 +0500

    back to work

---
+++ Data Structures and Algorithms/New_Dreama/src/numberPlate/Main.java
@@ -0,0 +1,20 @@
+package numberPlate;
+
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+        File imageFile = new File("eurotext.tif");
+        ITesseract instance = new Tesseract();  // JNA Interface Mapping
+        // ITesseract instance = new Tesseract1(); // JNA Direct Mapping
+        instance.setDatapath("tessdata"); // path to tessdata directory
+
+        try {
+            String result = instance.doOCR(imageFile);
+            System.out.println(result);
+        } catch (TesseractException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
