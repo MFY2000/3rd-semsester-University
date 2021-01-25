8aec61ee6f96c23b92c382795ed93c52e926bac7
MFY2000 <FA19BSSE0014@MAJU.EDU.PK>
2021-01-25 23:44:51 +0500

    25/1/2021 last commit ahmed ka error sol

--- Data Structures and Algorithms/New_Dreama/src/numberPlate/Main.java
+++ Data Structures and Algorithms/New_Dreama/src/numberPlate/Main.java
@@ -5,16 +5,49 @@ import java.io.File;
 public class Main {
 
     public static void main(String[] args) {
-        File imageFile = new File("eurotext.tif");
-        ITesseract instance = new Tesseract();  // JNA Interface Mapping
-        // ITesseract instance = new Tesseract1(); // JNA Direct Mapping
-        instance.setDatapath("tessdata"); // path to tessdata directory
+        System.out.println((new Main()).isEmailValide("fahad.yahya14@gmail.com"));
+    }
 
-        try {
-            String result = instance.doOCR(imageFile);
-            System.out.println(result);
-        } catch (TesseractException e) {
-            System.err.println(e.getMessage());
-        }
+    public void MakeEmailRed(boolean Exist,String Msg) {
+        if (!EmailID.getStyleClass().contains("error"))
+            EmailID.getStyleClass().add("error");
+
+        Invalid.setVisible(!Exist);
+        AlreadyExist.setVisible(Exist);
+    }
+
+    public void isEmailValide(String Email){
+        String[] CompanyName = {"yahoo", "gmail", "hotmail"};
+        String[] CompanyPath = {"org", "com"};
+
+        if (Email.contains("@")) {
+            String company = Email.substring(Email.lastIndexOf("@") + 1);
+            if (company.length() != 0) {
+                int iend = company.indexOf(".");
+                if (iend != -1) {
+                    String emailCompany = company.substring(0, iend);
+                    String com_org = company.substring(company.lastIndexOf(".") + 1);
+                    if (emailCompany.equals(CompanyName[0]) || emailCompany.equals(CompanyName[1]) || emailCompany.equals(CompanyName[2])) {
+                        if (com_org.equals(CompanyPath[0]) || com_org.equals(CompanyPath[1])) {
+                            String path_two = "C:\\Tawny\\Users\\";
+                            path_two = path_two + Email;
+                            File Dir2 = new File(path_two);
+                            if (!Dir2.exists())
+                                System.out.println("Hi");
+//                                AlreadyExist.setVisible(false);
+                            else {
+                                MakeEmailRed(true,"Already Exist Dir");
+//                                CreatedSuccessfully.setVisible(false);
+                            }
+                        }//"Invalid sytnx (.org,com)"
+                        else MakeEmailRed(false,"Invalid sytnx (.org,com)");
+                    }
+                    else MakeEmailRed(false,"Invalid sytnx email type ("+company.toString()+")");
+                }
+                else MakeEmailRed(false,"Missing sytnx (.org,com)");
+            } // company name is emtpy
+            else MakeEmailRed(false,"Missing after @ sign 408 empty");
+        } // na @ available ho na dot
+        else  MakeEmailRed(false,"Missing @ sign");
     }
 }
